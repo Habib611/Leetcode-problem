22e4934d5469158d14e624f75596d795df20d929
Habib611 <mdalhabib759@gmail.com>
2025-09-12 23:38:47 +0600

    solution of duplicate

---
+++ 217-contains-duplicate.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/contains-duplicate/
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+        sort(nums.begin(),nums.end());
+        int l=nums.size();
+        for(int i=0;i<l-1;i++)
+        {
+            if(nums[i]==nums[i+1])
+            {
+                return true;
+            }
+        }
+        return false;
+
+        
+    }
+};
